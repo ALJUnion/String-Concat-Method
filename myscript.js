ef579520bf8fa98a88dbd2fc0d3e4811c20df848
ALJUnion <58676296+ALJUnion@users.noreply.github.com>
2020-03-16 20:43:12 -0400

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,16 @@
+
+let greeting = prompt("Hello ");
+let name2 = " Bart Simpson here";
+
+let combined = greeting.concat(name2);
+
+function myfunction(){
+    let greeting;
+    let name2;
+}
+if(greeting === "hello"){
+    text = "Bart simpson is Awesome"
+}
+
+
+console.log(combined);
